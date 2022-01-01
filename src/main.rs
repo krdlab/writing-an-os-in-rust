a7a340a50c3f25e3817d59bcc09eb8633106e226
krdlab <krdlab@gmail.com>
2022-01-02 01:25:57 +0900

    feat: add no_main attribute and _start function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![no_std]
-
-fn main() {}
+#![no_main]
 
 use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
