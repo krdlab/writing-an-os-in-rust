c8537250d706e6247f87277f04de888744d82438
krdlab <krdlab@gmail.com>
2022-01-04 07:57:01 +0900

    test: add integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,23 +9,16 @@ use mini_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     mini_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
