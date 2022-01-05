f9fbdabdb9fa424a6a0b41c823164a32a15387c9
krdlab <krdlab@gmail.com>
2022-01-05 23:45:30 +0900

    feat: CPU exception (int3)

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,14 @@ use mini_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    mini_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
