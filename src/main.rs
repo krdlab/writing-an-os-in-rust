b7f76dcc380a92b76dd58407aef3f30fe527e502
krdlab <krdlab@gmail.com>
2022-01-16 19:05:23 +0900

    feat: add page fault handler

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,22 @@ pub extern "C" fn _start() -> ! {
     // unsafe {
     //     *(0xdeadbeef as *mut u64) = 42;
     // }
+    // let ptr = 0x20517e as *mut u32;
+    // unsafe {
+    //     let x = *ptr;
+    // }
+    // println!("read worked");
+    // unsafe {
+    //     *ptr = 42;
+    // }
+    // println!("write worked");
+
+    // use x86_64::registers::control::Cr3;
+    // let (level_4_page_table, _) = Cr3::read();
+    // println!(
+    //     "Level 4 page table at: {:?}",
+    //     level_4_page_table.start_address()
+    // );
 
     #[cfg(test)]
     test_main();
