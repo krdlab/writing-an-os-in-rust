d7d9d795992ea468b88879c426157a7eac970fc7
krdlab <krdlab@gmail.com>
2022-01-17 22:29:32 +0900

    feat: convert from page table entry to physical memory mapping address

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,37 @@
 #![test_runner(mini_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use mini_os::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use mini_os::memory::active_level_4_table;
+    use x86_64::VirtAddr;
+
     println!("Hello World{}", "!");
 
     mini_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    for (i, entry) in l4_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("L4 Entry {}: {:?}", i, entry);
+
+            use x86_64::structures::paging::PageTable;
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys_mem_offset + phys.as_u64();
+            let l3_table: &PageTable = unsafe { &*virt.as_mut_ptr() };
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("L3 Entry {}: {:?}", i, entry);
+                }
+            }
+        }
+    }
 
     // x86_64::instructions::interrupts::int3();
     // unsafe {
