735172cdfb5e13399e800188af7f1231c91254b6
krdlab <krdlab@gmail.com>
2022-01-18 13:29:58 +0900

    feat: translate addresses

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use mini_os::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use mini_os::memory::active_level_4_table;
+    use mini_os::memory::translate_addr;
     use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
@@ -19,21 +19,16 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     mini_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-
-            use x86_64::structures::paging::PageTable;
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys_mem_offset + phys.as_u64();
-            let l3_table: &PageTable = unsafe { &*virt.as_mut_ptr() };
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     // x86_64::instructions::interrupts::int3();
