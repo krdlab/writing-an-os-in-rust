e30c7a88947226c150e27afbe52ec532111f29c8
krdlab <krdlab@gmail.com>
2022-01-20 22:24:22 +0900

    feat: create a kernel heap with allocator crate

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use mini_os::println;
@@ -14,6 +14,7 @@ use mini_os::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use mini_os::allocator;
     use mini_os::memory::{self, BootInfoFrameAllocator};
     use x86_64::VirtAddr;
 
@@ -25,7 +26,28 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let x = Box::new(41);
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut v = Vec::new();
+    for i in 0..500 {
+        v.push(i);
+    }
+    println!("v at {:p}", v.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     // let page = Page::containing_address(VirtAddr::new(0));
     // let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
