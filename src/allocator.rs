3f02b9a10ec061d0089ff9a6872bd32dd0f43c39
krdlab <krdlab@gmail.com>
2022-01-27 13:28:12 +0900

    feat: add LinkedListAllocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,4 +1,4 @@
-use self::bump::BumpAllocator;
+use linked_list::LinkedListAllocator;
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
@@ -7,6 +7,7 @@ use x86_64::{
 };
 
 pub mod bump;
+pub mod linked_list;
 
 pub struct Locked<A> {
     inner: spin::Mutex<A>,
@@ -29,7 +30,7 @@ fn align_up(addr: usize, align: usize) -> usize {
 }
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100KiB
